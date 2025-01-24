c60372a6a81b41ffb5602572c96e05e86068796e
unknown <robin.kirchen@web.de>
2025-01-24 14:42:10 +0100

    created input field and button to add Items to the list

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+function addToList() {
+
+}
+
+</script>
+
+<template>
+  <main>
+    <h2>ToDoList:</h2>
+    <form>
+      <input type="text" id="entry">
+      <button @click="addToList">Add item</button>
+    </form>
+    <ul></ul>
+    
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
