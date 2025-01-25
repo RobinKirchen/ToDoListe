e74498ddf50e19d43ef3f523588d06adbeb6cf44
unknown <robin.kirchen@web.de>
2025-01-26 00:19:44 +0100

    added dropdown to select priority and refactored the input into a form element

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { ref, computed } from 'vue';
 
-let todos = ref([]);
-let newTodo = ref("");
+
 let id = 0;
-let hideCompleted = ref(false);
+const todos = ref([]);
+const newTodo = ref("");
+const hideCompleted = ref(false);
+const selectedPriority = ref();
 
 
 const filteredTodos = computed(() => {
@@ -13,15 +15,12 @@ const filteredTodos = computed(() => {
 
 function addToList() {
 
-  if(newTodo.value.length === 0) {
-    return;
-  }
-
   todos.value.push(
     {
-    id: id, 
-    name: newTodo.value,
-    completed: false
+      id: id,
+      name: newTodo.value,
+      completed: false,
+      priority: selectedPriority
     }
   );
   id++;
@@ -39,24 +38,31 @@ function deleteEntry(entryId) {
 <template>
   <main>
     <h2>ToDoList:</h2>
-    <input type="text" id="entry" v-model="newTodo" placeholder="Add a new todo">
-    <button @click="addToList">Add item</button>
+    <form @submit.prevent="addToList">
+      <input type="text" id="entry" v-model="newTodo" placeholder="Add a new todo" required>
+      <label>Priority:</label>
+      <select v-model="selectedPriority" required>
+        <option value="High">High</option>
+        <option value="Medium">Medium</option>
+        <option value="Low">Low</option>
+      </select>
+      <input type="submit" value="Add Todo">
+    </form>
 
-      <div v-if="todos.length > 0" v-for="entry in filteredTodos">
-        <div class="todo-entry">
-          <input type="checkbox" v-model="entry.completed">
-          <span class="todo-text">{{ entry.name }}</span>
-          <button @click="deleteEntry(entry.id)" class="delete">Delete</button>
-        </div>
+    <div v-if="todos.length > 0" v-for="entry in filteredTodos">
+      <div class="todo-entry" :class="entry.priority">
+        <input type="checkbox" v-model="entry.completed">
+        <span class="todo-text">{{ entry.name }}</span>
+        <button @click="deleteEntry(entry.id)" class="delete">X</button>
       </div>
+    </div>
     <div v-else>Add an entry to your list</div>
-    <button @click="hideCompleted = !hideCompleted">{{hideCompleted ? "Show All" : "Hide completed"}}</button>
+    <button @click="hideCompleted = !hideCompleted">{{ hideCompleted ? "Show All" : "Hide completed" }}</button>
 
   </main>
 </template>
 
 <style scoped>
-
 header {
   line-height: 1.5;
 }
@@ -76,9 +82,10 @@ header {
 }
 
 main .todo-entry {
-  display: flex;               
-  align-items: center;          
-  justify-content: space-between;  /* Distribute space between the checkbox the text and delete button. For equal space distribution on all sides use space-around instead*/
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  /* Distribute space between the checkbox the text and delete button. For equal space distribution on all sides use space-around instead*/
   font-size: large;
   width: 100%;
   margin-bottom: 5px;
@@ -86,11 +93,16 @@ main .todo-entry {
 
 main .todo-text {
   margin-left: 5px;
-  flex-grow: 1;                 /* Allow the text to take up remaining space */
+  flex-grow: 1;
+  /* Allow the text to take up remaining space */
   text-align: left;
 }
 
-main .delete{
+main .High {
+  background-color: red;
+}
+
+main .delete {
   float: right;
   border-color: blueviolet;
   border-width: 1px;
